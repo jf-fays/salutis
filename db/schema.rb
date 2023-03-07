0ae22d17eb2d2931c7b29efb15f15d2ffa35f512
Veronica Novikova <veronica.novikova@gmail.com>
2023-03-07 16:45:28 +0100

    medicals created

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,29 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_07_144518) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_07_154302) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "daily_takes", force: :cascade do |t|
+    t.string "day"
+    t.string "day_time"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "medical_procedures", force: :cascade do |t|
+    t.string "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "medicines", force: :cascade do |t|
+    t.string "name"
+    t.string "NR"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
---
+++ test/models/medical_procedure_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class MedicalProcedureTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
