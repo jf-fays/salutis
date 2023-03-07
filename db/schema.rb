e7f34b2135c53636869d00d360efd02fd53b25c9
jean-francois fays <fays.jf@gmail.com>
2023-03-07 17:37:54 +0100

    consultation model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_07_154328) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_07_163651) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "chatrooms", force: :cascade do |t|
+    t.bigint "patient_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["patient_id"], name: "index_chatrooms_on_patient_id"
+  end
+
+  create_table "consultations", force: :cascade do |t|
+    t.date "date"
+    t.bigint "user_id", null: false
+    t.bigint "patient_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["patient_id"], name: "index_consultations_on_patient_id"
+    t.index ["user_id"], name: "index_consultations_on_user_id"
+  end
 
   create_table "daily_takes", force: :cascade do |t|
     t.string "day"
@@ -32,13 +49,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_07_154328) do
     t.string "NR"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
-
-  create_table "chatrooms", force: :cascade do |t|
-    t.bigint "patient_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["patient_id"], name: "index_chatrooms_on_patient_id"
   end
 
   create_table "patients", force: :cascade do |t|
@@ -72,4 +82,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_07_154328) do
   end
 
   add_foreign_key "chatrooms", "patients"
+  add_foreign_key "consultations", "patients"
+  add_foreign_key "consultations", "users"
 end
