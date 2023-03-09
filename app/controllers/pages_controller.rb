ba89472224a9fbee4eda764c8ac9e0ca913f8e6a
jean-francois fays <fays.jf@gmail.com>
2023-03-10 00:31:03 +0100

    dashboard in progress

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,5 +7,14 @@ class PagesController < ApplicationController
   def dashboard
     @users = current_user
     @patients = current_user.patients
+    if params[:query].present?
+      sql_query = <<~SQL
+        patients.first_name @@ :query
+        OR patients.last_name @@ :query
+        OR patients.social_security_number @@ :query
+      SQL
+    else
+      @patients = Patient.all
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   devise_for :users
   root to: "pages#home"
-  get '/users/:id', to: 'pages#dashboard'
+  get '/users/:id', to: 'pages#dashboard', as: 'user'
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
