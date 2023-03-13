8cd93cb586ab5216e05e6c6fbfd13adbf5e41135
jean-francois fays <fays.jf@gmail.com>
2023-03-13 17:39:59 +0100

    searchbar fix

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,10 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import "bootstrap"
+
+$("select").select2({
+  theme: "bootstrap-5",
+  containerCssClass: "select2--large", // For Select2 v4.0
+  selectionCssClass: "select2--large", // For Select2 v4.1
+  dropdownCssClass: "select2--large",
+});
